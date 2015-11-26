ffc32a173c737cfa1d392f920b06ad7523987f71
HP - Pawel Zawada <zawadapawelmail@gmail.com>
2015-11-26 08:33:29 +0100

    Functions Geüpdate - toTitleCase - Naam en achternaam wordt nu gekapitaliseerd

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,6 +1,11 @@
 /**
  * Created by HP on 19-11-2015.
  */
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g,
+        function(txt){
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
 }
--- scripts/jscript.js
+++ scripts/jscript.js
@@ -1,13 +1,13 @@
 /*** Created by HP on 11-11-2015.*/
 var naam = prompt("Uw naam hier");
-naam = capitalizeFirstLetter(naam);
+naam = toTitleCase(naam);
 var date = new Date();
 var dd = date.getDate();
 var mm = date.getMonth()+1;
 var yyyy = date.getFullYear();
 var datum = dd + '/' + mm + '/' + yyyy;
 var model = prompt("Wat is het merk en type van uw modem?");
-model = capitalizeFirstLetter(model);
+model = toTitleCase(model);
 document.writeln('<div class="panel panel-success">' + '<div class="panel-heading">' + '<h1 class="panel-title">'+ 'Klant naam: ' + naam + '</h1>' + '</div>' + '<div class="panel-body">');
 document.writeln("Klant belt op: " + datum + "<br><br>");
 document.writeln("Modem van klant: " + model + "<br><br>");
